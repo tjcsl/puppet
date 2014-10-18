1af396921008ee4fe676b8781e16a1d58c4a1078
Samuel Damashek <samuel.damashek@gmail.com>
2014-10-18 19:42:35 -0400

    Fix service name

--- modules/cups/manifests/init.pp
+++ modules/cups/manifests/init.pp
@@ -5,7 +5,7 @@ class cups {
     file { '/etc/cups/client.conf':
         ensure => present,
         source => 'puppet:///modules/cups/client.conf',
-        notify => Service[cups]
+        notify => Service[cupsd]
     }
     service { 'cupsd':
         ensure => running,
