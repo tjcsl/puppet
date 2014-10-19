08394f949bb3bddb8368d729fc04b5f0ac5db6bb
Samuel Damashek <samuel.damashek@gmail.com>
2014-10-19 16:28:53 -0400

    Path fix

--- modules/math/manifests/init.pp
+++ modules/math/manifests/init.pp
@@ -3,11 +3,11 @@ class math {
         ensure => file,
         source => 'puppet:///modules/math/Mathematica_10.0.1_LINUX.sh'
     }
-    file { '/tmp/yaourt-tmp-root/aur-mathematica/':
+    file { '/tmp/yaourt-tmp-root/aur-mathematica/src/':
         ensure => directory,
         require => Class[aur]
     }
-    file { '/tmp/yaourt-tmp-root/aur-mathematica/Mathematica_10.0.1_LINUX.sh':
+    file { '/tmp/yaourt-tmp-root/aur-mathematica/src/Mathematica_10.0.1_LINUX.sh':
         ensure => link,
         target => '/opt/Mathematica_10.0.1_LINUX.sh',
         require => [File['/tmp/yaourt-tmp-root/aur-mathematica/'],File['/opt/Mathematica_10.0.1_LINUX.sh']]
