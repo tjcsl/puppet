de9f82e2a5694e7e22cd23d91afc0bef77f5ab93
Samuel Damashek <samuel.damashek@gmail.com>
2014-10-19 01:41:53 -0400

    nss_ldap package

--- modules/nsswitch/manifests/init.pp
+++ modules/nsswitch/manifests/init.pp
@@ -1,4 +1,8 @@
 class nsswitch {
+    package { 'nss_ldap':
+        ensure => latest,
+        require => Class[aur]
+    }
     file { '/etc/nsswitch.conf':
         ensure => file,
         source => 'puppet:///modules/nsswitch/nsswitch.conf'
